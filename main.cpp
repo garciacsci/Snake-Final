a42fd5df42c252398555b60ebb78a95fc261f6b4
garc897 <62458331+garc897@users.noreply.github.com>
2020-05-12 15:51:10 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,45 +20,22 @@
 using namespace std;
 
 // 1. Declare the structure object boardInfo with file scope
-info boardInfo;
+info gameInfo;
+
+
+//----------------------------------------------------------------------------//
+
 
 // Define main function
 int main(int argc, char **argv)
 {
     // Declare variables
-    ifstream fin;
+    //ifstream fin;
     ofstream fout;
     
     /* 2. Declare positional variables (if needed) to determine 
           if the snake has eaten an apple.*/
     int snakeRow, snakeCol, appleRow, appleCol;
-    
-    
-    
-
-    /*
-                // THIS MAY BE UNECESSARY
-    
-    // Open saved game data
-    fin.open("gameData.txt");
-    
-    // Make sure input file opened
-    if(fout.fail())
-    {
-        cout << "'gameData.txt' failed to open...";
-        exit(EXIT_FAILURE);        
-    }
-    
-    // Import saved game data
-    
-        // FILL THIS IN LATER
-    
-    
-    // Close input
-    fin.close();
-    
-    */
-
     
     // 3. Open a file for saving relevant game information.
     fout.open("gameData.txt");
@@ -68,9 +45,11 @@ int main(int argc, char **argv)
     {
         cout << "'gameData.txt' failed to open...";
         exit(EXIT_FAILURE);
-    }
+    }   
     
     // 4. Initialize the XCurses library using a function
+    cout << "calling on initXcurses...\n";    
+    
     initXCurses(argc, argv);
     
     // 5. Initialize the colors using a function (if needed)
@@ -82,30 +61,42 @@ int main(int argc, char **argv)
     srand(clock());
     
     // 7. Initialize the gameInfo object using a function. 
-    initGameInfo(fout);
+    cout << "\ncalling on initGameInfo...\n";
     
-    // 8. Print a Welcome Screen using a function    
+    initGameInfo(fout);                
+    
+    // 8. Print a Welcome Screen using a function       
     welcome();
     
     // 9. Declare snake and apple class objects
-    Snake snake;
+    Snake snake();
     Apple apple;
     
+    
+    // GET RID OF THIS LATER
+    nodelay(stdscr, FALSE);
+    
     // 10. Create a loop to play the game:
-    while(1)
-    {
+    while(TRUE)
+    {                
         // a. Print the game board.
         printBoard();
-        
+                                                
         // b. Move the snake with a class member function.
-        moveSnake();
+        snake.move();        
         
         /* c. Change the speed of the game up or down, 
               or quit the game, if necessary. */
         
                 // RESEARCH CODE FOR THIS LATER
         
+        
         // d. Get the snake head position with a class member function.
+        snake.getHeadPos(snakeRow, snakeCol);                
+        
+        
+        //  GET RID OF THIS LATER
+        getch();
         
         
         /* e. Compare the snake position with the walls, tail, 
@@ -113,10 +104,10 @@ int main(int argc, char **argv)
               (use functions, as necessary) */
         
         
-        // f. End the game if the snake collides with itself or a wall
+            // f. End the game if the snake collides with itself or a wall
         
         
-        // g. Grow the snake and generate a new apple if necessary
+            // g. Grow the snake and generate a new apple if necessary
         
         
         // h. Update the number of apples eaten, length of the snake, etc
@@ -136,9 +127,10 @@ int main(int argc, char **argv)
     // Close the game window
     endwin();
     
+    // Close file
+    fout.close();
+    
     // 13. End the program. 
     exit(EXIT_SUCCESS);                
 }
-
-
 //----------------------------------------------------------------------------//
