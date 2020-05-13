d0317d887720d6886ae5f1a215cfb75b491c2aba
garc897 <62458331+garc897@users.noreply.github.com>
2020-05-12 23:50:47 -0700

    Update apple.hpp

--- apple.hpp
+++ apple.hpp
@@ -3,25 +3,35 @@
 #ifndef APPLE_HPP
 #define APPLE_HPP
 
-using namespace std;
-
-class apple
+class Apple
 {
 public:
-            
-    int appleR;
-    int appleC;
-    apple();
-    void newApple();
-    void getApplePos(int &appleRow, int &appleCol);
+    // Default Constructor
+     Apple();
+
+     // Constructor with color initialization
+     Apple(int c);
+
+     // Function to help create a new apple
+     void newApple(int c);
+
+     // Accessor function to determine apple position
+     void getApplePos(int &Y, int &X);
+     
+     // Increments number of apples eaten
+     void incrementApplesEaten();
+     
+private:
+    int appleRow, appleCol, applesEaten, color;    
     
+    
+    /*
+     * You may add data members and 
+     * member functions to the class, as appropriate. Recall that the class 
+     * declaration occurs in apple.hpp, and contains the declarations for the 
+     * function prototypes and the data members only. The class member function 
+     * definitions are placed in apple.cpp
+     */
 };
-//objects
-apple apple1, apple2,  apple3;
-
-
-
 
 #endif /* APPLE_HPP */
-
-
