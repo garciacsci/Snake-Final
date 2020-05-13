e8cdfc7c63ed7be30e207908170d7a9b39ae6aaa
mauricioromero55 <64935772+mauricioromero55@users.noreply.github.com>
2020-05-13 15:16:41 -0700

    Update color.hpp

--- color.hpp
+++ color.hpp
@@ -3,6 +3,10 @@
 #ifndef COLOR_H
 #define COLOR_H
 
+// Initialize Color Attributes
+void initColors(void);
 
+// Set Color Function used to Set the Color of an Object
+void color(int row, int col, int x); 
 
 #endif /* COLOR_H */
