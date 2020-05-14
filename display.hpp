348deff02598f7652d2c38a5b4613adc06a71acb
garc897 <62458331+garc897@users.noreply.github.com>
2020-05-14 14:53:21 -0700

    Update display.hpp

--- display.hpp
+++ display.hpp
@@ -1,4 +1,11 @@
-/* Stores display function prototypes */
+/* Snake display.hpp */
+
+/* 
+ * File:    display.hpp
+ * Author:  Christopher Garcia
+ * Purpose: Stores display function prototypes
+ * Date:    14 May 2020
+ */
 
 #ifndef DISPLAY_HPP
 #define DISPLAY_HPP
@@ -24,10 +31,14 @@ void keypress();
 // Post Difficulty Menu
 void difficultyMenu();
 
+// Posts player menu (worm, snake, or both);
+void playerMenu();
+
 // Call appropriate post game screen
 void postGame(Res x);
 
 // Print the results of the game
-void results(int &bestLength, int &bestEaten, int userLength, int userEaten);
+void results(int &bestLength, int &bestEaten, int snakeLength, int wormLength, \
+        int userEaten);
 
 #endif /* DISPLAY_HPP */
