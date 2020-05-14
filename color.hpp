fc781aa4b911b100f5e07cdbb054368e67eb7706
garc897 <62458331+garc897@users.noreply.github.com>
2020-05-14 14:52:54 -0700

    Update color.hpp

--- color.hpp
+++ color.hpp
@@ -1,4 +1,11 @@
-/* Stores color code constants and color function prototypes */
+/* Snake color.hpp */
+
+/* 
+ * File:    color.hpp
+ * Author:  Christopher Garcia
+ * Purpose: Stores color code constants and color function prototypes
+ * Date:    14 May 2020
+ */
 
 #ifndef COLOR_HPP
 #define COLOR_HPP
